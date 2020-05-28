f09a50d51925c80b0f344cbcbdf1889b4bd45a31
samuel Nyarkoh <cyelons45@yahoo.com>
2020-05-27 21:52:55 -0400

    updated userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ const AppError = require('./../utils/appError');
 const factory = require('./../controllers/handlerFactory');
 var multer = require('multer');
 const sharp = require('sharp');
-var upload = multer({dest: 'public/img/users'});
+// var upload = multer({dest: 'public/img/users'});
 
 // var storage = multer.diskStorage({
 //   destination: function (req, file, cb) {
@@ -25,19 +25,19 @@ function multerFilter(req, file, cb) {
   if (file.mimetype.startsWith('image')) {
     cb(null, true);
   } else {
-    cb(new AppError('Not an image. Please upload only images.', 400), false);
+    cb(new AppError('Not an image!. Please upload only images.', 400), false);
   }
 }
 
-var upload = multer({storage, fileFilter: multerFilter});
+var upload = multer({storage: storage, fileFilter: multerFilter});
 
 exports.uploadUserPhoto = upload.single('photo');
 exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
-  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpg`;
   await sharp(req.file.buffer)
     .resize(500, 500)
-    .toFormat('jpeg')
+    .toFormat('jpg')
     .jpeg({quality: 90})
     .toFile(`public/img/users/${req.file.filename}`);
   next();
